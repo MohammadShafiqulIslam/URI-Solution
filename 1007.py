44a783790e9e29f82e8f4d80f46a7fcc14f93e9d
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-09 19:12:32 +0600

    URI 1007 - Difference

---
+++ 1007.py
@@ -0,0 +1,11 @@
+A = int(input());
+B = int(input());
+C = int(input());
+D = int(input());
+DIFERENCA = (A*B - C*D);
+print("DIFERENCA = %d"%DIFERENCA);
+
+'''
+#Visit for more solution
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1007-solution-difference.html
+'''
