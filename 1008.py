c753962e0a9461bb7d1f941b5994f77fe0fbf80a
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-09 19:13:46 +0600

    URI 1008 - Salary

---
+++ 1008.py
@@ -0,0 +1,11 @@
+EmployeeNumber = int(input());
+WorkHours = int(input());
+Price = float(input());
+Salary = WorkHours * Price;
+print("NUMBER = %d"%EmployeeNumber);
+print("SALARY = U$ %.2f"%Salary);
+
+'''
+#Visit for more solution
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1008-solution-salary.html
+'''
