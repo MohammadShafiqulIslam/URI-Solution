431fbb36ddd1ebb3c82e24d8a29bf332dce967f4
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-09 19:17:49 +0600

    URI 1010 - Simple Calculate

---
+++ 1010.py
@@ -0,0 +1,8 @@
+A,B,C = map(float, input().split());
+D,E,F = map(float, input().split());
+Value = (B*C) + (E*F);
+print("VALOR A PAGAR: R$ %.2f"%Value);
+
+'''
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1010-solution-simple-calculate.html
+'''
