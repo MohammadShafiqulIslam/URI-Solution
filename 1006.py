fb9ace70b23ed1574b6a73ef2d2d4be63fa2da8d
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-09 19:11:14 +0600

    URI 1006 - Average 2

---
+++ 1006.py
@@ -0,0 +1,10 @@
+A = float(input());
+B = float(input());
+C = float(input());
+MEDIA = (A*2 + B*3 + C*5)/(2+3+5);
+print("MEDIA = %.1f"%MEDIA);
+
+'''
+#Visit for more solution
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1006-solution-average-2.html
+'''
