d991bbdaf8e5f90f0592c46aa2737264b0010170
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-09 19:16:01 +0600

    URI 1009 - Salary with Bonus

---
+++ 1009.py
@@ -0,0 +1,9 @@
+Name = input();
+Salary = float(input());
+Sold = float(input());
+Salary = Salary + (Sold*15)/100;
+print("TOTAL = R$ %.2f"%Salary);
+
+'''
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1001-extremely-basic.html
+'''
