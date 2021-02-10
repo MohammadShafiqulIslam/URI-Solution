6e1a4c7913b2815b3852036dee2fab1378b05606
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-11 00:33:04 +0600

    URI 1012 - Area

---
+++ 1012.py
@@ -0,0 +1,18 @@
+A,B,C = map(float, input().split());
+pi = 3.14159;
+TRIANGULO = 0.5*(A*C);
+CIRCULO = pi*C*C;
+TRAPEZIO = ((A+B)/2)*C;
+QUADRADO = B*B;
+RETANGULO = (A * B);
+
+print("TRIANGULO: %.3f"%TRIANGULO);
+print("CIRCULO: %.3f"%CIRCULO);
+print("TRAPEZIO: %.3f"%TRAPEZIO);
+print("QUADRADO: %.3f"%QUADRADO);
+print("RETANGULO: %.3f"%RETANGULO);
+
+'''
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1012-solution-area.html
+'''
+
