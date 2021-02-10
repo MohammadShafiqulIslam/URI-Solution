8e1cf32a9636afa4de3b8142a8d912997e82ae5a
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-11 00:34:39 +0600

    URI 1013 - The Greatest

---
+++ 1013.py
@@ -0,0 +1,12 @@
+A,B,C = map(int, input().split());
+if A>B and A>C:
+    print("%d eh o maior"%A);
+elif B>A and B>C:
+    print("%d eh o maior"%B);
+else:
+    print("%d eh o maior"%C);
+
+'''
+#https://urisolutions.blogspot.com/2020/04/uri-problem-1013-solution-the-greatest.html
+'''
+
