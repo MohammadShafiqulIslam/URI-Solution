0324c84d000c861bc5985a78c8459d1aac46ca30
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-15 01:14:56 +0600

    URI 1026 - To Carry or not to Carry

---
+++ 1026.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        list1 = input().split()
+        X, Y = list1
+        print(int(X) ^ int(Y))
+    except:
+        break
+
+'''
+https://urisolutions.blogspot.com/2020/04/uri-problem-1026-solution-To-Carry-or-not-to-Carry.html
+'''
