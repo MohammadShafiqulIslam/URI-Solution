549ed666b206db13913c5db9d7a722f2b0880486
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-15 00:40:36 +0600

    URI  1020 - Age in Days

---
+++ 1020.py
@@ -0,0 +1,12 @@
+D = int(input())
+Y = int(D/365)
+D = int(D%365)
+M = int(D/30)
+D = D%30
+print("%d ano(s)"%Y)
+print("%d mes(es)"%M)
+print("%d dia(s)"%D)
+
+'''
+https://urisolutions.blogspot.com/2020/04/uri-problem-1020-solution-age-in-days.html
+'''
