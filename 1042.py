6e4f88d9175dbf58f8a6a80c765e999db634f1b9
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-16 00:21:47 +0600

    URI  1042 - Simple Sort

---
+++ 1042.py
@@ -0,0 +1,10 @@
+X,Y,Z = map(int,input().split())
+list = [X,Y,Z]
+list.sort()
+print("%d\n%d\n%d"%(list[0],list[1],list[2]))
+print("\n%d\n%d\n%d"%(X,Y,Z))
+
+
+'''
+https://urisolutions.blogspot.com/2020/04/uri-problem-1042-solution-simple-sort.html
+'''
