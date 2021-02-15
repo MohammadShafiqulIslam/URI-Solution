b7c4648f8865decebdc753dc5cd7a224db6e18ee
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-16 00:16:08 +0600

    URI 1038 - Snack

---
+++ 1038.py
@@ -0,0 +1,8 @@
+X,Y = map(int,input().split())
+Item = {1:4.0,2:4.5,3:5.0,4:2.0,5:1.5}
+X = float(Item[X])*Y
+print("Total: R$ %.2f"%X)
+
+'''
+https://urisolutions.blogspot.com/2020/04/uri-problem-1038-solution-snack.html
+'''
