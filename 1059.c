e82906189a58a749e39801286d620e4c3e1a5a9a
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-18 00:54:28 +0600

    URI  1059 - Even Numbers

---
+++ 1059.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int i;;
+    for(i=1; i<=100; i++)
+    {
+        if(i%2 ==0)
+            printf("%d\n",i);
+    }
+ 
+    return 0;
+}
+
+//https://urisolutions.blogspot.com/2020/04/uri-problem-1059-solution-even-number.html
