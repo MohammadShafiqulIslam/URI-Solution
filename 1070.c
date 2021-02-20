69fa7d67b2dca3fe87bd3a48a6be491e7c2cefd0
MohammadShafiqulIslam <mdshafiq.cse@gmail.com>
2021-02-20 23:40:20 +0600

    URI 1070 Six Odd Numbers

---
+++ 1070.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int x,i,c=0;
+    scanf("%d",&x);
+    while(c<=5)
+    {
+        if(x%2 != 0)
+        {
+            printf("%d\n",x);
+            c++;
+        }
+        x++;
+    }
+    return 0;
+}
+
+'''
+https://urisolutions.blogspot.com/2020/04/uri-problem-1070-solution-six-odd-numbers.html
+'''
